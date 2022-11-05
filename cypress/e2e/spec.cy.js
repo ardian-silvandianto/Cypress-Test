9d2efd6d95cb97b0c7d5851ef39d25a5507fe4f4
asilvandianto <ardian.silvandianto@gmail.com>
2022-11-05 22:05:11 +1300

    Initial set up

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,12 @@
-describe('empty spec', () => {
-  it('passes', () => {
+describe('Accessing Flybuys website', () => {
+  it('Opens the web', () => {
     cy.visit('https://www.flybuys.co.nz')
+
+    cy.get('#hamburger_btn').click()
+
+    cy.get('#hamburger_query').type('asdfadsfadsfadsadsf {Enter}')
+
+    cy.get('.no-results').contains('sorry there are no results')
+
   })
 })
